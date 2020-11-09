883ba48f365de9e012ee003574b1aa98cb843cf5
Bruce McClure <brucemcc90@gmail.com>
2020-11-09 14:41:28 +1100

    basic_strut_code

--- 3.organising_data_with_struts/main.go
+++ 3.organising_data_with_struts/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // This is the definition of what a person is.
 type person struct {
 	firstName string
@@ -7,5 +9,7 @@ type person struct {
 }
 
 func main() {
-
+	// bruce := person{"Bruce", "McClure"} // This relys 100% on the order for assignemnt
+	bruce := person{firstName: "Bruce", lastName: "McClure"} // This doesn't rely on order
+	fmt.Println(bruce)
 }
